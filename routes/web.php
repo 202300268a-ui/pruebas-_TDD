4ee9a1ee03172580a67b9b52b7716cc1a3560b7d
202300268a-ui <202300268a@utea.edu.pe>
2026-05-25 21:33:25 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
 
 Route::get('/', function () {
     return view('welcome');
+
+
+Route::get('events',[EventController::class,'index'])->name('events.index');
+Route::post('events',[EventController::class,'store'])->name('events.store');
+Route::put('events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+
+
 });
