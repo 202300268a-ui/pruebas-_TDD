780d1150a5a9d5e955c74b57f03a8ae9731c46af
202300268a-ui <202300268a@utea.edu.pe>
2026-05-25 23:27:04 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 
 Route::get('/', function () {
     return view('welcome');
-
 });
- 
+
 Route::get('events',[EventController::class,'index'])->name('events.index');
 Route::post('events',[EventController::class,'store'])->name('events.store');
+Route::put('events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
