a77cf2ac78a6b1913a1c518180261df8cc03b9b7
202300268a-ui <202300268a@utea.edu.pe>
2026-05-25 20:32:28 -0500

    Update 2026_05_26_011556_create_events_table.php

--- database/migrations/2026_05_26_011556_create_events_table.php
+++ database/migrations/2026_05_26_011556_create_events_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('featured');
+            $table->timestamp('date');
+            $table->time('time');
+            $table->string('location');
             $table->timestamps();
         });
     }
